e34274daab3fc5c3e6b62ab85d5bfcb6f83b971c
unknown <C:\Users\Oli4ka\AppData\Roaming\The Bat!>
2014-05-28 00:49:11 +0300

    Olga: Added header. Created navigation routes.

--- ActionInUkraine/ActionInUkraine.Web/App/viewmodels/shell.js
+++ ActionInUkraine/ActionInUkraine.Web/App/viewmodels/shell.js
@@ -3,6 +3,13 @@
 
     return {
         router: router,
+        homeRoute: router.visibleRoutes[0],
+        hederMenu: router.visibleRoutes.slice(1, 5),
+        ideaMenu: router.visibleRoutes.slice(5, 8),
+        actions: router.visibleRoutes.splice(8),
+        navigateToHome: function() {
+            router.navigateTo('#/home');
+        },
         activate: function () {
             return router.activate('home');
         }
